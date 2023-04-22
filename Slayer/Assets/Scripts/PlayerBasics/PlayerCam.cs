84d97bb0294509a62c470a5c074f7fca2e390106
jvpts11 <jvptsyt11@gmail.com>
2023-04-22 20:16:42 +0100

    Movement Fixed
    but smh jump don't work

--- Slayer/Assets/Scripts/PlayerBasics/PlayerCam.cs
+++ Slayer/Assets/Scripts/PlayerBasics/PlayerCam.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +5,14 @@ using UnityEngine;
 public class PlayerCam : MonoBehaviour
 {
 
-    public float sensitivity = 120f;
+    public float sensX;
+    public float sensY;
 
     public Transform orientation;
 
-    float mouseX;
-    float mouseY;
     float xRotation;
     float yRotation;
+
 
     void Start()
     {
@@ -21,18 +20,19 @@ public class PlayerCam : MonoBehaviour
         Cursor.visible = false;
     }
 
+    
     void Update()
     {
-        
-        mouseX = Input.GetAxisRaw("Mouse X") * sensitivity * Time.deltaTime;
-        mouseY = Input.GetAxisRaw("Mouse Y") * sensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
         yRotation += mouseX;
 
         xRotation -= mouseY;
+
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-        orientation.localRotation = Quaternion.Euler(0,yRotation, 0f);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
--- Slayer/Assets/Scripts/PlayerBasics/PlayerMovement.cs
+++ Slayer/Assets/Scripts/PlayerBasics/PlayerMovement.cs
@@ -4,26 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [Header("Movement")]
     public float moveSpeed;
+    public float groundDrag;
 
-    float horizontalInput;
-    float verticalInput;
+    [Header("Jumping")]
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplayer;
 
+    [Header("Ground Checking")]
+    public LayerMask whatIsGround;
     public Transform orientation;
+
+    [Header("Misc")]
+    public float playerHeight;
+
+    [Header("KeyBinds")]
+    public KeyCode jumpkey = KeyCode.Space;
+
+    bool isGrounded;
+    bool readyToJump = true;
+
+    float horizontal;
+    float vertical;
+
+    Vector3 dir;
+
     Rigidbody rb;
-
-    Vector3 moveDir;
-
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
     }
 
+    
     void Update()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
         PlayerInput();
+        CheckIsGrounded(isGrounded);
+        SpeedControl();
     }
 
     private void FixedUpdate()
@@ -31,15 +54,66 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
-    private void PlayerInput()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-    }
-
     private void MovePlayer()
     {
-        moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
+        dir = orientation.forward * vertical + orientation.right * horizontal;
+
+        rb.AddForce(dir.normalized * moveSpeed * 10f,ForceMode.Force);
+
+        if(isGrounded)
+        {
+            rb.AddForce(dir.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else if(!isGrounded)
+        {
+            rb.AddForce(dir.normalized * moveSpeed * 10f * airMultiplayer, ForceMode.Force);
+        }
+    }
+
+    private void CheckIsGrounded(bool isGrounded)
+    {
+        if(isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void PlayerInput()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(jumpkey) && readyToJump && isGrounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump),jumpCooldown);
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatvel = new Vector3(rb.velocity.x,0f, rb.velocity.z);
+
+        if(flatvel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatvel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x,rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
