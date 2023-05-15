10e4680e8a3faa77a27cc48aedd4827498c8b480
jvpts11 <jvptsyt11@gmail.com>
2023-05-15 15:42:25 +0100

    Working in enemy
    it is almost done

---
+++ Slayer/Assets/Scripts/EnemyRelated/EnemyData.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "Entity/Enemy")]
+public class EnemyData : ScriptableObject
+{
+    [Header("Enemy Info")]
+    public string enemyName;
+    public float health;
+    public float speed;
+
+    [Header("Bullet")]
+    public GameObject bullet;
+    public float bulletDamage;
+    public float bulletSpeed;
+
+    [Header("Range")]
+    public float sightRange;
+    public float attackRange;
+
+    [Header("Booleans")]
+    public bool playerInAttackRange;
+    public bool playerInSightRange;
+    public bool walkpointSet;
+    public bool alreadyAttacked;
+
+    [Header("Attack Info")]
+    public NavMeshAgent agent;
+    public Transform player;
+    public float timeBetweenAttacks;
+
+    [Header("Positioning Info")]
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+}
